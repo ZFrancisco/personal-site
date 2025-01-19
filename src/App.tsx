e392419c796b9afa150bfcf11603469811c51e2a
ZackaryFrancisco <zplesha@berkeley.edu>
2025-01-19 01:11:08 -0800

    first skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 
+
 export default function App() {
   return (
     <MantineProvider theme={theme}>
