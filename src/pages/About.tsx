958b274d5b18ab344f69fce3f4cce72d4872022a
ZFrancisco <zackfrancisco50@gmail.com>
2025-01-23 19:26:42 -0800

    done

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div style={{fontFamily: 'Times'}}>
         <h1 style={{fontSize: '2rem', marginLeft: '15px'}}>About:</h1>
-            <p style={{marginLeft: '10px'}}>My name is Zackary Francisco and I am a sophomore at UC berkeley
+            <p style={{marginLeft: '10px'}}>My name is Zackary Francisco and I am a sophomore at UC Berkeley
                  studying Computer Science, Data Science, and Mathematics.</p>
     </div>
   )
